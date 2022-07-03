d3bf56e8b0b0e42c1843867679ffb84a6fbaa1dd
leejae honey <ljh23270@naver.com>
2022-07-04 01:07:39 +0900

    pug extends

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,9 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+  return res.render("watch");
 };
 export const edit = (req, res) => {
-  return res.send("Edit");
+  return res.render("edit");
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
