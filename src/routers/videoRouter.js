fcebf739e726d621e385a1d2de6fd860b780c81f
leejae honey <ljh23270@naver.com>
2022-07-04 21:37:46 +0900

    set postEdit

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  upload,
+  deleteVideo,
+  getEdit,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload); //순서 중요
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+//순서 중요
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
