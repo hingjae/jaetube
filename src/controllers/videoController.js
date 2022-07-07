2ffd3ab0c79c82b70f0800ab51c2895487f8d8f9
leejae honey <ljh23270@naver.com>
2022-07-07 13:10:40 +0900

    static func

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,11 +39,8 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
-
   return res.redirect(`/videos/${id}`);
 };
 
@@ -57,9 +54,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 //mongoose == bridge for nodeJS, mongoDB
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 40 },
   description: { type: String, required: true, minLength: 5 },
@@ -10,6 +11,14 @@ const videoSchema = new mongoose.Schema({
     rating: { type: Number, default: 0, required: true },
   },
 });
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+// upload middleware.
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
