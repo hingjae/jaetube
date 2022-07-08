846a5652986cea4f61e6263403f1eca78ed571c6
leejae honey <ljh23270@naver.com>
2022-07-08 18:45:50 +0900

    user.db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -11,7 +11,7 @@ app.use(logger);
 app.set("view engine", "pug"); // pug사용을 위한 setting
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true })); //express가 hrml의 form을 이해할 수 있도록함.
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
